e45ad8b2e56fe0ca14af28bf5125ed43ba39321b
Aleksey <134486666+Aleky63@users.noreply.github.com>
2025-07-04 16:27:05 +0300

    01_04.07

--- go/main.go
+++ go/main.go
@@ -1,93 +1,154 @@
+// package main
+
+// import (
+// 	"bufio"
+// 	"errors"
+// 	"fmt"
+// 	"log"
+// 	"os"
+// 	"strings"
+// 	"unicode"
+// )
+// //
+// func GetInput() (str string, err error) {
+// 	scanner := bufio.NewScanner(os.Stdin)
+// 	scanner.Scan()
+
+//     if err = scanner.Err(); err != nil {
+// 		return "", fmt.Errorf("scanner.Err(): %w", err)
+// 	}
+
+// 	str = scanner.Text()
+
+// 	if str == "" {
+// 		return "", errors.New("String is empty")
+// 	}
+// 	if strings.TrimSpace(str) == "" {
+// 		return "", errors.New("String contains only spaces")
+// 	}
+// 	return
+// }
+
+// func CountCharacters(text string) (letters, digits, spaces, punctuation int){
+
+// for _, c := range text {
+// 		switch {
+// 		case c == ' ':
+// 			spaces++
+// 		case unicode.IsLetter(c):
+// 			letters++
+// 		case unicode.IsDigit(c):
+// 			digits++
+// 		case unicode.IsPunct(c):
+// 			punctuation++
+// 		}
+// 	}
+// 	return
+// }
+// func DisplayResults(letters, digits, spaces, punctuation int){
+// fmt.Printf("Letters count: %d\n", letters)
+// 	fmt.Printf("Digits count: %d\n", digits)
+// 	fmt.Printf("Spaces count: %d\n", spaces)
+// 	fmt.Printf("Punctuation count: %d\n", punctuation)
+//   }
+// func main(){
+//     text, err := GetInput()
+
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	DisplayResults(CountCharacters(text))
+//     fmt.Print("Please enter text: ")
+// }
+
 package main
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
-	"math/rand/v2"
+	"log"
 	"os"
-	"strconv"
-	"strings"
+	"unicode"
 )
 
-var ErrUserEndGame = errors.New("user ehd game")
+// Ввод текста
+// Создайте функцию GetInput() (string, error), которая запрашивает у пользователя ввод текста и возвращает его для дальнейшего анализа. Если функция вернет ошибку, программу необходимо завершить.
 
-func generateRandomNumber(min,max int) int{
-    return rand.IntN(max-min +1)+ min
+
+func GetInput() (str string, err error) {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("Введите текст:")
+	 if !scanner.Scan(){
+         fmt.Println()
+	 
+    if err = scanner.Err(); err != nil {
+		return "", fmt.Errorf("scanner.Err(): %w", err)
+	}
+return "", errors.New("unable to read input")
+}
+return scanner.Text(), nil
+}
+	
+// Подсчет символов
+// Реализуйте функцию CountCharacters(text string) (letters, digits, spaces, punctuation int), которая принимает текст и возвращает количество:
+
+// букв (letters)
+// цифр (digits)
+// пробелов (spaces)
+// знаков препинания (punctuation)
+
+func CountCharacters(text string) (letSmalls, letBigs, digits, spaces, punctuation int){
+
+for _, c := range text {
+		switch {
+		
+		case unicode.IsUpper(c):
+			letBigs++
+		case unicode.IsLower(c) :
+			letSmalls++
+		case unicode.IsDigit(c):
+			digits++
+		case unicode.IsPunct(c):
+			punctuation++
+		case unicode.IsSpace(c):
+			spaces++		
+		}
+	}
+	return
 }
 
-func getUserInput(min, max int) (int, error){
-    var input string
-    fmt.Printf("Ваше предположение (либо, для завершения, введите слово выход):")
-fmt.Scanln(&input)
+// Вывод результатов
 
-if strings.ToLower(input) == "выход" {
-    return 0, ErrUserEndGame
-}
-number, err := strconv.Atoi((input))
-if err != nil {
-   return 0, fmt.Errorf("invalid input: %w", err) 
-}
+// Напишите функцию DisplayResults(letters, digits, spaces, punctuation int), которая принимает результаты анализа и выводит их на экран в удобочитаемом формате. Например:
 
-if number < min || number > max {
-        return 0, fmt.Errorf("число должно быть в диапазоне от %d до %d", min, max)
-    }
+// Количество букв: 50
+// Количество цифр: 10
+// Количество пробелов: 5
+// Количество знаков препинания: 3
 
-return number, nil
-}
+func DisplayResults( letBigs, letSmalls, digits, spaces, punctuation int){
 
-func playGame() error {
-    min := 1
-    max := 100
-   randomNumber := generateRandomNumber(min, max)
-  count := 0
-   
-fmt.Println("Компьютер загадал случайное число от 1 до 100 включительно. Угадайте его!")
+// fmt.Printf(`Количество заглавных букв: %d
+// Количество прописных букв: %d
+// Количество цифр: %d
+// Количество пробелов: %d
+// Количество знаков препинания: %d`, letSmalls, letBigs, digits, spaces, punctuation )
 
-   for {
-    input, err := getUserInput(min,max)
-    if err !=nil{
-        if err == ErrUserEndGame{
-            return  err
-} 
-fmt.Println("Неккоректное число")
-continue
-        }    
-    count++   
-    
-    if input < randomNumber {
-         fmt.Println("Загаданное число больше.")
-    } else if input > randomNumber{
-fmt.Println("Загаданное число меньше.")
-    } else {
-       fmt.Printf("Правильно! Вы угадали число с %d попытки.\n", count) 
-       break
-    }
-   }
- return nil
-}
+    fmt.Printf("letBigs count: %d\n", letBigs)
+    fmt.Printf("letSmalls count: %d\n", letSmalls)
+	fmt.Printf("Digits count: %d\n", digits)
+	fmt.Printf("Spaces count: %d\n", spaces)
+	fmt.Printf("Punctuation count: %d\n", punctuation)
+  }
+func main(){
+    text, err := GetInput()
 
-func printGameEnd(){
-   fmt.Println("Спасибо за игру! До свидания!") 
-}
+	if err != nil {
+		log.Fatal(err)
+	}
 
-func main () {
-   for {
-    if err := playGame(); err != nil  {
-        if err == ErrUserEndGame {
-            printGameEnd()
-            break   
-        }
-fmt.Printf("Возникла ошибка во время игры: %v", err)
-os.Exit(1)     
-    }
-
-    var playAgain string
-    fmt.Println("Хотите сыграть еще раз? (если хотите, напишите слово да): нет")
-    fmt.Scanln(&playAgain)
-    if playAgain != "да"{
-    printGameEnd()
-        break
-    }
-   }
-  
+	DisplayResults(CountCharacters(text))
+    fmt.Print("Please enter text: ")
 }
