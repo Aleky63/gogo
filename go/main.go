1e9836bae6aeb74f3210408f08682aeb5013650e
Aleksey <134486666+Aleky63@users.noreply.github.com>
2025-07-08 17:27:35 +0300

    01_08.07

--- go/main.go
+++ go/main.go
@@ -1,44 +1,40 @@
 package main
 
-import "fmt"
-
-func SumNeighbors(arr [10]int) (newArr [10]int) {
-    for i := range arr {
-        switch {
-        case i == 0:
-            newArr[0] = arr[1]
-        case i == 9:
-            newArr[9] = arr[8]
-        default:
-            newArr[i] = arr[i-1] + arr[i+1]
-        }
-    }
-    return
-}
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-    // Пример массива
-    arr := [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-    
-    fmt.Println("Исходный массив:")
-    fmt.Println(arr)
-    
-    result := SumNeighbors(arr)
-    
-    fmt.Println("Результат (сумма соседей):")
-    fmt.Println(result)
-    
-    // Проверим каждый элемент
-    fmt.Println("\nПодробно:")
-    for i, val := range result {
-        switch {
-        case i == 0:
-            fmt.Printf("newArr[%d] = arr[1] = %d\n", i, val)
-        case i == 9:
-            fmt.Printf("newArr[%d] = arr[8] = %d\n", i, val)
-        default:
-            fmt.Printf("newArr[%d] = arr[%d] + arr[%d] = %d + %d = %d\n", 
-                i, i-1, i+1, arr[i-1], arr[i+1], val)
+	arr := [7]int{1, 9, 6, 3, 1, 9, 8}
+	fmt.Println("Cекретный код:", func() string {
+	
+		return generateCode(arr)
+	}())
+
+}
+
+func generateCode(arr [7]int) string {
+	min := arr[0]
+    max := arr[0]
+     for _, v := range arr {
+        if v < min {
+            min = v
+        }
+        if v > max {
+            max = v
         }
     }
+
+    var middle string
+    for _, v := range arr {
+        prefix := "O"
+        if v%2 == 0 {
+            prefix = "E+/+E"
+        }
+        middle += prefix + strconv.Itoa(v) 
+    }
+
+    
+    return strconv.Itoa(min)  + middle +  strconv.Itoa(max)
 }
