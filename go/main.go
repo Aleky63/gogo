66b683ff88408af67a243998aab9035ce22c26b5
Aleksey <134486666+Aleky63@users.noreply.github.com>
2025-07-19 16:10:10 +0300

    02_19.07

--- go/main.go
+++ go/main.go
@@ -1,17 +1,55 @@
 package main
 
-import (
+import (	
+	"errors"
 	"fmt"
 )
 
 
-func main ()  {
-s1 := []int	{1, 2, 3, 4}
-s2 := []int	{11, 22, 33, 44, 555}
-		
-copied :=copy(s2,s1)
-	fmt.Println(s2)
-	fmt.Print(copied)
+
+func intersectSlices(s1[]int, s2[]int) ([]int, error){
+
+
+if s1 ==nil || s2 ==nil {
+	return nil,errors.New("slices cannot be nil")
+}
+
+
+s3 :=[]int{}
+i, j := 0, 0
+
+
+    for i < len(s1) && j < len(s2)  {
+	if s1[i] == s2[j] {
+
+    s3 = append (s3, s1[i])
 
 	
+	i++
+	j++
+
+} else if s1[i] < s2[j]{
+		i++
+	}else {
+		j++
+	}
+}
+
+return s3,nil
+
+}
+
+
+func main ()  {
+s1 := []int{1, 2, 3, 4, 5}
+s2 := []int{3, 4, 5, 6, 7}
+
+
+res, err := intersectSlices(s1,s2)
+
+if err != nil{
+	fmt.Println(err)
+	return
+}
+fmt.Println(res)
  }
