aef04ee76601a6113ac6053b73a2f9cb995f0534
Aleksey <134486666+Aleky63@users.noreply.github.com>
2025-08-08 15:06:03 +0300

    01_08.08

--- go/main.go
+++ go/main.go
@@ -2,96 +2,98 @@ package main
 
 import (
 	"fmt"
-	"maps"
 	"slices"
+	"strings"
 )
 
+func countFriends(data map[string][]string) map[string]int {
+	friendsCount := make(map[string]int, len(data))
+	names := make([]string, 0, len(data))
 
-
-func printRecommendations(movies map[string]map[string]float64) {
-
-	genresMoviesGood := make(map[string]map[string]float64)
-
-	for genre, films := range movies {
-		goodMovies :=  make(map[string]float64)
-
-		for filmName, rating := range films {
-			if rating >= 7.0 {
-				goodMovies[filmName] = rating
-			}
-		}
-
-		if len(goodMovies) > 0 {
-			genresMoviesGood[genre] = goodMovies
-		}
+	for name, friends := range data {
+		friendsCount[name] = len(friends)
+		names = append(names, name)
 	}
-	genres := slices.Collect(maps.Keys(genresMoviesGood))
-	slices.Sort(genres)
+	slices.Sort(names)
 
-for _, genre := range genres {
-	films := genresMoviesGood[genre]
-		movieNames := slices.Collect(maps.Keys(films))
+	fmt.Println("Количество друзей:")
+	for _, name := range names {
+		fmt.Printf("%s: %d\n", name, friendsCount[name])
+	}
+	return friendsCount
+}
 
-slices.SortFunc(movieNames, func(a, b string) int {
-		
-			ratingA := films[a]
-			ratingB := films[b]
-
-          if ratingA != ratingB {
-				if ratingA > ratingB {
-					return -1
-				}
-				return 1
-			}
-
-			if a < b {
-				return -1
-			} else if a > b {
-				return 1
-			}
-			return 0
-		})
-		for _, movieName := range movieNames {
-			rating := films[movieName]
-			fmt.Printf("  %s (%.2f)\n", movieName, rating)
-		}
-		fmt.Println() 
+func commonFriends(data map[string][]string, user1, user2 string) []string {
+	friends1, ok1 := data[user1]
+	friends2, ok2 := data[user2]
+	if !ok1 || !ok2 {
+		fmt.Printf("Один из пользователей (%s или %s) не найден\n", user1, user2)
+		return nil
 	}
 
+	common := make([]string, 0, min(len(friends1), len(friends2)))
+	for _, friend := range friends1 {
+		if slices.Contains(friends2, friend) {
+			common = append(common, friend)
+		}
+	}
+	slices.Sort(common)
+	return common
+}
+
+func mostPopularUsers(data map[string][]string) ([]string, int) {
+	maxFriends := 0
+	popularUsers := make([]string, 0)
+
+	// Первый проход: находим максимальное количество друзей
+	for _, friends := range data {
+		if len(friends) > maxFriends {
+			maxFriends = len(friends)
+		}
+	}
+
+	// Второй проход: собираем пользователей с максимальным количеством друзей
+	for user, friends := range data {
+		if len(friends) == maxFriends {
+			popularUsers = append(popularUsers, user)
+		}
+	}
+
+	slices.Sort(popularUsers)
+	return popularUsers, maxFriends
 }
 
 func main() {
-movies := map[string]map[string]float64{
-		"Экшен": {
-			"Матрица":           8.7,
-			"Терминатор 2":      8.5,
-			"Джон Уик":          7.4,
-			"Быстрый и яростный": 6.5,
-		},
-		"Драма": {
-			"Побег из Шоушенка": 9.3,
-			"Крестный отец":     9.2,
-			"Форрест Гамп":      8.8,
-			"Зеленая миля":      8.6,
-		},
-		"Комедия": {
-			"Один дома":         7.7,
-			"Маска":            6.9,
-			"Такси":            7.0,
-		},
-		"Ужасы": {
-			"Звонок":           6.2,
-			"Пила":             6.1,
-		},
-		"Фантастика": {
-			"Интерстеллар":     8.6,
-			"Начало":           8.85,
-			"Бегущий по лезвию": 8.1,
-		},
+	friendsData := map[string][]string{
+		"Алексей":  {"Иван", "Сергей", "Елена"},
+		"Иван":     {"Алексей", "Дмитрий", "Мария"},
+		"Сергей":   {"Алексей", "Елена"},
+		"Дмитрий":  {"Иван", "Елена", "Ольга"},
+		"Елена":    {"Алексей", "Сергей", "Дмитрий"},
+		"Мария":    {"Иван", "Ольга"},
+		"Ольга":    {"Дмитрий", "Мария"},
+		"Анна":     {"Петр"},
+		"Петр":     {"Анна", "Сергей"},
+		"Светлана": {"Иван", "Елена"},
 	}
-	
-	fmt.Println("Рекомендации фильмов (рейтинг 7.0+):")
-	fmt.Println("====================================")
-	printRecommendations(movies)
+
+	// 1. Подсчет количества друзей
+	countFriends(friendsData)
+
+	// 2. Поиск общих друзей
+	user1, user2 := "Иван", "Елена"
+	common := commonFriends(friendsData, user1, user2)
+	if len(common) > 0 {
+		fmt.Printf("Общие друзья между %s и %s: %s\n", 
+			user1, user2, strings.Join(common, ", "))
+	} else {
+		fmt.Printf("У %s и %s нет общих друзей\n", user1, user2)
+	}
+
+	// 3. Поиск самых популярных пользователей
+	popular, max := mostPopularUsers(friendsData)
+	fmt.Printf("Самые популярные пользователи: %s (друзей: %d)\n", 
+		strings.Join(popular, ", "), max)
 }
-// https://stepik.org/lesson/1517684/step/1?unit=1537967
+
+//https://stepik.org/lesson/1535976/step/1?unit=1556545
