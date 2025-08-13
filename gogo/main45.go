091e7ecbc31130ef159144542a29c5c53b14bef8
Aleksey <134486666+Aleky63@users.noreply.github.com>
2025-08-13 15:52:03 +0300

    06_13.08

--- gogo/main45.go
+++ gogo/main45.go
@@ -94,4 +94,33 @@ movies := map[string]map[string]float64{
 	fmt.Println("====================================")
 	printRecommendations(movies)
 }
-// https://stepik.org/lesson/1517684/step/1?unit=1537967
+// https://stepik.org/lesson/1517684/step/1?unit=1537967package main
+
+import "fmt"
+
+type Event struct {
+	Title string
+	Date string
+    Location string
+}
+
+
+func createGoEvent() Event{
+    event := Event {
+	Title: "День рождения Golang",
+   Date: "10 ноября 2009",
+    Location: "GoogleLand",
+}
+	return event
+}
+
+
+func main46() {
+	
+ res:= createGoEvent()
+
+	fmt.Println(res)
+	fmt.Printf("Событие: %s\nДата: %s\nМесто: %s\n", res.Title, res.Date, res.Location)
+}
+
+// https://stepik.org/lesson/1500866/step/4?unit=1520983
