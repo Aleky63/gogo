6c3337f7f7970b88bf9585a82e58069ad613d9d9
Aleksey <134486666+Aleky63@users.noreply.github.com>
2025-08-13 14:48:48 +0300

    01_13.08

--- go/main.go
+++ go/main.go
@@ -1,145 +1,48 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-	"strings"
-)
-
-type Address struct {
-	ID         int
-	
-	City       string
-	Street     string
-	PostalCode int
-}
-
-type Product struct {
-	ID          int
-	Name        string
-	Description string
-	Price       int
-	Category    string
-	Brand       string
-	Rating      float64
-	Reviews     int
-}
-
-type CartItem struct {
-	Product  Product
-	Quantity int
+import "fmt"
+type Name struct{
+	First string
+	Last string
 }
 
 type User struct {
-	Name    string
-	 Email string
-	 Phone    string
-	Address Address
-	Cart    []CartItem
+	Name Name
+	Year int
 }
 
-func printInfo(user User) error {
-	if user.Name == "" ||  user.Phone == "" || user.Address.City == "" || user.Address.Street == "" {
-		return errors.New("ошибка: не все обязательные данные пользователя переданы")
-	}
-	fmt.Printf("Покупатель %s. Телефон: %s. Адрес: г. %s, %s.\n",
-		user.Name, user.Phone, user.Address.City, user.Address.Street)
-	return nil
-}
 
-func hasElectronics(user User) bool {
-	for _, item := range user.Cart {
-		if item.Product.Category == "Электроника" {
-			return true
-		}
-	}
-	return false
-}
+func(u User) Greet() {
 
-func expensiveItems(user User) []string {
-var res [] string
-for _, item := range user.Cart {
-	if item.Product.Price >= 10000{
-		res = append(res, item.Product.Name)
-	}
-	
+fmt.Printf("Z %s %s %d   fdfddfdfdf.\n", u.Name.First, u.Name.Last, u.Year)
 }
-return res
-}
-func totalSum(user User) int {
-	 var sum int
-	for _, item:= range user.Cart {
-		sum += item.Product.Price * int(item.Quantity)
-	}
-	return sum
-}
-
 
 func main() {
-	user := User{
-		Name:  "Иван Петров",
-		Phone: "+7 999 123-45-67",
-		 Email: "ssss.ivanov@example.com",
-		Address: Address{
-			ID:         1,
-			City:       "Yeysk",
-			Street:     "Улица Высоцкого",
-			PostalCode: 101000,
+	user := User {
+		Name: Name{
+			First:"AAAAAA",
+	        Last : "dddddd",
 		},
-		Cart: []CartItem{
-			{
-				Product: Product{
-					ID:          1,
-					Name:        "Ноутбук",
-					Description: "Мощный ноутбук для работы и игр",
-					Price:       59990,
-					Category:    "Электроника",
-					Brand:       "Brand A",
-					Rating:      4.5,
-					Reviews:     120,
-				},
-				Quantity: 1,
-			},
-			{
-				Product: Product{
-					ID:          2,
-					Name:        "Смартфон",
-					Description: "Современный смартфон с отличной камерой",
-					Price:       29990,
-					Category:    "Электроника",
-					Brand:       "Brand B",
-					Rating:      4.7,
-					Reviews:     200,
-				},
-				Quantity: 2,
-			},
-			{
-				Product: Product{
-					ID:          3,
-					Name:        "Наушники",
-					Description: "Беспроводные наушники с шумоподавлением",
-					Price:       1990,
-					Category:    "Аудио",
-					Brand:       "Brand C",
-					Rating:      4.3,
-					Reviews:     80,
-				},
-				Quantity: 1,
-			},
+		Year: 19999999,
+	}
+	user2 := User {
+		Name: Name{
+			First:"OOOOOO",
+	        Last : "aa",
 		},
+		Year: 99б
 	}
 
-	if err := printInfo(user); err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	if hasElectronics(user) {
-		fmt.Println("Пользователь является покупателем электроники.")
-	} else {
-		fmt.Println("Пользователь не является покупателем электроники.")
-	}
-	expItems := expensiveItems(user)
-	fmt.Printf("Товары в корзине, где цена 10000 и более: [%s].\n", strings.Join(expItems, ", "))
-	fmt.Printf("Общая сумма покупки: %.2d руб.\n", totalSum(user))
+	fmt.Printf("%+v\n", user)
+	user.Greet()
+	user.Name.First = "YYYYYYYYYYYYYYYYYYYYYYYYYYYYY"
+	user.Greet()
+	user2.Greet()
 }
+
+
+
+
+
+	
+
