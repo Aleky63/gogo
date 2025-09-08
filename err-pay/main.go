d321ff2f7a3a7534eb17f7e67e9283e87eb0028a
Aleksey <134486666+Aleky63@users.noreply.github.com>
2025-09-08 15:38:06 +0300

    01_08.09

--- err-pay/main.go
+++ err-pay/main.go
@@ -75,9 +75,24 @@ func HandlePayments() error {
 
 func main() {
 	if err := HandlePayments(); err != nil {
-		// Обработать ошибки, которые могут произойти при вызове HandlePayments
+		// Проверяем на неподдерживаемый метод
+		if errors.Is(err, ErrUnsupportedPaymentMethod) {
+			fmt.Println("Ошибка: неподдерживаемый метод платежа.")
+		} else {
+			// Проверяем на ошибку нехватки средств
+			var insufficientFundsErr *InsufficientFundsError
+			if errors.As(err, &insufficientFundsErr) {
+				fmt.Printf("Ошибка: недостаточно средств. %s\n", insufficientFundsErr.Error())
+			} else {
+				// Все остальные ошибки
+				fmt.Printf("Неизвестная ошибка: %v.\n", err)
+			}
+		}
 		os.Exit(1)
 	}
 
 	fmt.Println("Платеж успешно обработан!")
 }
+
+// https://chat.qwen.ai/c/6e1e2bd9-43a6-4d79-8bb8-738a47deb9a8
+// https://stepik.org/lesson/1538502/step/2?unit=1559099
