0744359aa8296aac1b60e098f78ae515b3dadfaa
Aleksey <134486666+Aleky63@users.noreply.github.com>
2025-09-11 17:20:13 +0300

    01_11.09

--- gonew/main.go
+++ gonew/main.go
@@ -2,8 +2,67 @@ package main
 
 import "fmt"
 
-func main() {
+type Wallet struct {
+	Cash int
+}
 
-	fmt.Println("Hello, DFDSFDVVVFBFBFB")
+// Pay implements Payer.
+func (w *Wallet) Pay(int) error {
+	panic("unimplemented")
+}
+
+type Card struct {
+	Balance    int
+	ValidUntil string
+	Cardholder string
+	CVV        string
+	Number     string
+}
+
+func (c *Card) Pay(amount int) error {
+	if c.Balance < amount {
+		return fmt.Errorf("Мало денег card")
+	}
+	c.Balance -= amount
+	return nil
+}
+
+type ApplePay struct {
+	Money   int
+	AppleID string
+}
+
+func (a *ApplePay) Pay(amount int) error {
+
+	if a.Money < amount {
+		return fmt.Errorf("Мало денег akk")
+	}
+	a.Money -= amount
+	return nil
+}
+
+type Payer interface {
+	Pay(int) error
+}
+
+func Buy(p Payer) {
+	err := p.Pay(10)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Спасибо за покупку через %T\n\n", p)
+}
+
+func main() {
+	myWallet := &Wallet{Cash: 100}
+	Buy(myWallet)
+
+	var myMoney Payer
+	myMoney = &Card{Balance: 100, Cardholder: "rvas"}
+	Buy(myMoney)
+
+	myMoney = &ApplePay{Money: 9}
+
+	Buy(myMoney)
 
 }
---
+++ gonew/main1.go
@@ -0,0 +1,46 @@
+// package main
+
+// import "fmt"
+
+// type Person struct {
+// 	Id   int
+// 	Name string
+// }
+
+// func (p *Person) SetName(name string) {
+// 	p.Name = name
+// }
+
+// type MySlice []int
+
+// func (sl *MySlice) Add(val int) {
+// 	*sl = append(*sl, val)
+// }
+// func (sl *MySlice) Count() int {
+// 	return len(*sl)
+// }
+
+// type Account struct {
+// 	Id   int
+// 	Name string
+// 	Person
+// }
+
+// func main1() {
+
+// 	pers := Person{1, "Vasiliy"}
+// 	pers.SetName(("Vasiliy Romanov"))
+// 	fmt.Println(pers)
+// 	fmt.Printf("updated person: %#v\n", pers)
+
+// 	var acc Account = Account{
+// 		Id:   3,
+// 		Name: "rrrrrrrrr",
+// 		Person: Person{
+// 			Id:   5,
+// 			Name: "ASSSSSSS",
+// 		},
+// 	}
+// 	acc.SetName("ROOOOOOO")
+// 	fmt.Printf(" %#v\n", acc)
+// }
