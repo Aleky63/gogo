3f22c9bac1f959e7b1e1b72ebe8567cdb6ded61a
Aleksey <134486666+Aleky63@users.noreply.github.com>
2025-09-30 11:53:49 +0300

    02_30.09

--- bogstash/go10/main.go
+++ bogstash/go10/main.go
@@ -6,14 +6,14 @@ import (
 	"github.com/fatih/color"
 )
 
-func getDrinkInfo(name string, drink string, price float64) {
+func printDrinkInfo(name string, drink string, price float64) {
 	red := color.New(color.FgRed).SprintFunc()
-	info := "%s's favorite drink is %s and its price is %s\n	"
+	info := "%s's favorite drink is %s and it's price is %s\n	"
 
 	fmt.Printf(info, name, drink, red(fmt.Sprintf("$%.2f", price)))
 
 }
 func main() {
-	getDrinkInfo("Tramp", "Chay", 4.66)
+	printDrinkInfo("Tramp", "Chay", 4.66)
 
 }
