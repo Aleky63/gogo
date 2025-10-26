e62a18c25ad8ad49df07e5682c825e037083ee4b
Aleksey <134486666+Aleky63@users.noreply.github.com>
2025-10-26 13:48:14 +0300

    01_26.10

--- newgo2/go1/main.go
+++ newgo2/go1/main.go
@@ -15,21 +15,37 @@ type Auto interface {
 }
 
 func (b BMW) StepOnGas() {
-
 	fmt.Println("Я Шкода! 55555!")
 }
-func (y YAZ) StepOnGas() {
 
+func (b BMW) StepOnBrake() {
+	magenta := color.New(color.FgHiMagenta).SprintFunc()
+	fmt.Println(" ")
+	fmt.Println(magenta("BMW braking erereree"))
+}
+func (b BMW) Bibi() {
+	green := color.New(color.BgHiGreen).SprintFunc()
+	fmt.Println(green(" ----  Я Шкода! bibibibibi!"))
+}
+
+func (y YAZ) StepOnGas() {
 	fmt.Println("Я YAZik! 22222!")
+}
+func (y YAZ) StepOnBrake() {
+	magenta := color.New(color.FgHiMagenta).SprintFunc()
+	fmt.Println(" ")
+	fmt.Println(magenta("YAZ braking yyyyyyyyy"))
 }
 
 func ride(auto Auto) {
 	red := color.New(color.FgHiRed).SprintFunc()
+
 	fmt.Println(red("Я водила с нижнего тагила"))
 	fmt.Println("Я садюсь в авто")
 	fmt.Println("Я жму на газ")
 	auto.StepOnGas()
 	auto.StepOnBrake()
+
 }
 
 func main() {
@@ -38,6 +54,7 @@ func main() {
 	yaz := YAZ{}
 
 	ride(bmw)
+	bmw.Bibi()
 	fmt.Println("")
 	ride(yaz)
 
