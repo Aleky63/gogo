89c308d7c5e05f7b6cfe8e9b2d4eac9fd81dab14
Aleksey <134486666+Aleky63@users.noreply.github.com>
2025-11-08 12:03:53 +0300

    01_08.11

--- newgo2/go5/main.go
+++ newgo2/go5/main.go
@@ -8,7 +8,7 @@ import (
 func nine(n int) int {
 	fmt.Println("Поход в шахту номер", n, "начался ...")
 
-	time.Sleep(1 * time.Second)
+	time.Sleep(2 * time.Second)
 
 	fmt.Println("Поход в шахту номер", n, "закончился.")
 	return 10
@@ -16,7 +16,11 @@ func nine(n int) int {
 
 func main() {
 	coal := 0
+	initTime := time.Now()
 	coal += nine(1)
 	coal += nine(2)
 	coal += nine(3)
+
+	fmt.Println("Добыли", coal, "угля!")
+	fmt.Println("Прошло времени:", time.Since(initTime))
 }
