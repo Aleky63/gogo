84062ec291a0ce48b00b9444f11e84bd4e769f82
Aleksey <134486666+Aleky63@users.noreply.github.com>
2025-11-08 18:59:05 +0300

    02_08.11

--- newgo2/go5/main.go
+++ newgo2/go5/main.go
@@ -5,21 +5,37 @@ import (
 	"time"
 )
 
-func nine(n int) int {
+func mine(transferPoint chan int, n int) {
+
 	fmt.Println("Поход в шахту номер", n, "начался ...")
 
-	time.Sleep(2 * time.Second)
+	time.Sleep(10 * time.Millisecond)
 
 	fmt.Println("Поход в шахту номер", n, "закончился.")
-	return 10
+
+	transferPoint <- 10
+	fmt.Println("Поход №", n, "сдал уголь.")
 }
 
 func main() {
+	transferPoint := make(chan int, 10)
 	coal := 0
+
 	initTime := time.Now()
-	coal += nine(1)
-	coal += nine(2)
-	coal += nine(3)
+
+	go mine(transferPoint, 1)
+	go mine(transferPoint, 2)
+	go mine(transferPoint, 3)
+	go mine(transferPoint, 4)
+
+	coal += <-transferPoint
+	time.Sleep(1 * time.Second)
+	coal += <-transferPoint
+	time.Sleep(1 * time.Second)
+	coal += <-transferPoint
+	time.Sleep(1 * time.Second)
+	coal += <-transferPoint
+	time.Sleep(1 * time.Second)
 
 	fmt.Println("Добыли", coal, "угля!")
 	fmt.Println("Прошло времени:", time.Since(initTime))
