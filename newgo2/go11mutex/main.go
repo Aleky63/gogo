7b0933cc348e592088f90cda000f6597c325fa2e
Aleksey <134486666+Aleky63@users.noreply.github.com>
2025-11-14 19:23:12 +0300

    02_14.11

--- newgo2/go11mutex/main.go
+++ newgo2/go11mutex/main.go
@@ -1,5 +1,45 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
+var likes int = 0
+var mtx sync.RWMutex
+
+func setLike(wg *sync.WaitGroup) {
+	defer wg.Done()
+	for i := 1; i < 2000000; i++ {
+		mtx.Lock()
+		likes++
+		mtx.Unlock()
+	}
+
+}
+func getLike(wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for i := 1; i < 2000000; i++ {
+		mtx.RLock()
+		_ = likes
+		mtx.RUnlock()
+	}
+}
+
+func main() {
+	wg := &sync.WaitGroup{}
+	initTime := time.Now()
+	for i := 1; i <= 10; i++ {
+
+		wg.Add(1)
+		go setLike(wg)
+	}
+	for i := 1; i <= 10; i++ {
+		wg.Add(1)
+		go getLike(wg)
+	}
+	wg.Wait()
+	fmt.Println("ВРЕМЯ ВЫПОЛНЕНИЯ:", time.Since(initTime))
 }
