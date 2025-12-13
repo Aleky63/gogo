6b56b4f6ac8e5a20dce385cdc8f4463706612b1b
Aleksey <134486666+Aleky63@users.noreply.github.com>
2025-12-13 17:30:53 +0300

    01_13.12

--- newgo2/go13/main.go
+++ newgo2/go13/main.go
@@ -2,11 +2,49 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/fatih/color"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	str := "Hello, Eysk!"
+	b := []byte(str)
+	_, err := w.Write(b)
+
+	if err != nil {
+		fmt.Println("ERROR;", err.Error())
+		return
+	}
+	fmt.Println("😍😍😍-OK-😍😍😍")
+}
+
+func cancelHandler(w http.ResponseWriter, r *http.Request) {
+
+	str := "Pay exit!"
+	b := []byte(str)
+	_, err := w.Write(b)
+
+	if err != nil {
+		fmt.Println("ERROR;", err.Error())
+		return
+	}
+	fmt.Println("🎈🎈🎈--OK--🎈🎈🎈")
+}
+
 func main() {
 	red := color.New(color.FgHiRed).SprintFunc()
-	fmt.Println(red("-🚕🚓🚕-"))
+	fmt.Println(red("--🚕🚓🚕--"))
+
+	http.HandleFunc("/default", handler)
+	http.HandleFunc("/pay", cancelHandler)
+
+	fmt.Println("Запускаю сервер🏦🏦🏦")
+	err := http.ListenAndServe(":9091", nil)
+	if err != nil {
+		fmt.Println("ERRoooooOR;", err.Error())
+
+	}
+
 }
