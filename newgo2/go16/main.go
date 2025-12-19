ee6dbf3bbe0188218130a922ae09f42a355ed7c4
Aleksey <134486666+Aleky63@users.noreply.github.com>
2025-12-19 17:44:33 +0300

    01_19.12

--- newgo2/go16/main.go
+++ newgo2/go16/main.go
@@ -1,18 +1,35 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
-	"strings"
+
+	// "strconv"
+	// "strings"
 	"sync"
 
 	"github.com/fatih/color"
 )
 
 type Payment struct {
-	Description string
-	USD         int
+	Description string `json:"description"`
+	USD         int    `json:"usd"`
+	FullName    string `json:"fullName"`
+	Address     string `json:"address"`
+	Color       string `json:"color"`
+	Flag        bool   `json:"flag"`
+	Array       []int  `json:"array"`
+}
+
+func (p Payment) Println() {
+	fmt.Println("Description:", p.Description)
+	fmt.Println("USD:", p.USD)
+	fmt.Println("FullName:", p.FullName)
+	fmt.Println("Address:", p.Address)
+	fmt.Println("Color:", p.Color)
+	fmt.Println("Flag:", p.Flag)
+	fmt.Println("Array:", p.Array)
 }
 
 var mtx = sync.Mutex{}
@@ -20,31 +37,37 @@ var money = 1000
 var paymenHistory = make([]Payment, 0)
 
 func payHandler(w http.ResponseWriter, r *http.Request) {
-	httpRequestBody, err := io.ReadAll(r.Body)
+	var payment Payment
 
-	if err != nil {
+	if err := json.NewDecoder(r.Body).Decode(&payment); err != nil {
+		fmt.Println("err:", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	httpRequestBodyString := string(httpRequestBody)
-	parts := strings.SplitN(httpRequestBodyString, ",", 2)
-	if len(parts) != 2 {
-		w.WriteHeader(http.StatusBadRequest)
-		return
+
+	payment.Println()
+
+	mtx.Lock()
+	if money-payment.USD >= 0 {
+		money -= payment.USD
 	}
+
+	paymenHistory = append(paymenHistory, payment)
+
+	red := color.New(color.FgHiRed).SprintFunc()
+	yellow := color.New(color.FgHiYellow).SprintFunc()
+
+	fmt.Println(red("money:", money))
+	fmt.Println(yellow("payment history:", paymenHistory))
+
+	mtx.Unlock()
 }
 
 func main() {
 
-	red := color.New(color.FgHiRed).SprintFunc()
-	fmt.Println(red("-----🚕🚓🚕-----"))
+	fmt.Println("-----I🚓I-----")
 
 	http.HandleFunc("/pay", payHandler)
-
-	// err := http.ListenAndServe(":1991", nil)
-	// if err != nil {
-	// 	fmt.Println("ERROR  during HTTP server operation:", err)
-	// }
 
 	if err := http.ListenAndServe(":9091", nil); err != nil {
 		fmt.Println("ERROR  during HTTP server operation:", err)
