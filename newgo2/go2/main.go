d8675185c5b4399f4b1ff824c701310b074bdbb1
Aleksey <134486666+Aleky63@users.noreply.github.com>
2026-01-28 17:57:33 +0300

    01_28.01

--- newgo2/go2/main.go
+++ newgo2/go2/main.go
@@ -19,8 +19,38 @@ type PaymentInfo struct {
  	s []PaymentInfo
 }
 
+
+
+ func (m *PaymentModuleWithSlice) AddInfo(info PaymentInfo) {
+	m.s = append(m.s, info)
+}
+func (m *PaymentModuleWithSlice) GetInfo(id int) PaymentInfo {
+	for _, info := range m.s {
+		if info.ID == id {
+			return info
+		}
+	}
+	return PaymentInfo{}
+}
+
+
+// --------------------------------------
+
 type PaymentModuleWithMap struct {
  	m map[int]PaymentInfo
+}
+
+func (m *PaymentModuleWithMap) AddInfo(info PaymentInfo) {
+	m.m[info.ID] = info
+ }
+
+
+ func (m *PaymentModuleWithMap) GetInfo(id int) PaymentInfo {
+	info, ok := m.m[id]
+	if !ok {
+		return PaymentInfo{}
+	}
+	return info
 }
 
 func main(){
