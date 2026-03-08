53dd9ed500003e19a730d9ec5f1905b7fb6ec20e
Aleksey <134486666+Aleky63@users.noreply.github.com>
2026-03-08 18:03:47 +0300

    01_08.03

---
+++ uprog/go3/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+type Player interface {
+	KickBall()
+}
+
+type FootballPlayer struct {
+	Power   int
+	Stamina int
+}
+
+func (f FootballPlayer) KickBall() {
+	shot := f.Power + f.Stamina
+
+	fmt.Printf("Я бью по мячу: %d\n", shot)
+}
+
+type Messi struct {
+	Power   int
+	Stamina int
+	Height  float64
+}
+
+func (m Messi) KickBall() {
+	shot := m.Power + m.Stamina
+	shot *= 5
+	fmt.Printf("Messi ростом: %.2f бьет по мячу: %d", m.Height, shot)
+}
+
+func main() {
+	players := make([]Player, 11)
+
+	for p := range players {
+		players[p] = FootballPlayer{
+			Power:   rand.Intn(10),
+			Stamina: rand.Intn(10),
+		}
+
+		if p == 10 {
+			players[p] = Messi{
+				Power:   rand.Intn(10),
+				Stamina: rand.Intn(10),
+				Height:  1.75,
+			}
+		}
+
+	}
+
+	for p := range players {
+		players[p].KickBall()
+	}
+}
