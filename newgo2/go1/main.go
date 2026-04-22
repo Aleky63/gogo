310054b13d122715bed38445bd610ae2d2045f54
Aleksey <134486666+Aleky63@users.noreply.github.com>
2026-04-22 18:48:10 +0300

    01_23.04

--- newgo2/go1/main.go
+++ newgo2/go1/main.go
@@ -6,63 +6,43 @@ import (
 	"github.com/fatih/color"
 )
 
-type Skoda struct{}
-type YAZ struct{}
-
 type Auto interface {
 	StepOnGas()
-	StepOnBrake()
-	Bibi()
+	// StepOnBrake()
+	// Bibi()
+}
+type (
+	Skoda struct{}
+	YAZ   struct{}
+	BMW   struct{}
+)
+
+func (b BMW) StepOnGas() {
+	fmt.Println("Я BMW! 55555!")
 }
 
-func (b Skoda) StepOnGas() {
-	fmt.Println("Я Шкода! 55555!")
+func (s Skoda) StepOnGas() {
+	fmt.Println("Я Шкода! 33333!")
 }
 
-func (b Skoda) StepOnBrake() {
-	magenta := color.New(color.FgHiMagenta).SprintFunc()
-	fmt.Println(" ")
-	fmt.Println(magenta("Skoda braking erereree"))
-}
-func (b Skoda) Bibi() {
-	green := color.New(color.BgHiGreen).SprintFunc()
-	fmt.Println(green(" ----  Я Шкода! bibibibibi!"))
-}
+// func (s YAZ) StepOnGas() {
+// 	fmt.Println("Я YAZ!  😍YYYYYYYYYYYYYYYYYYYYYYYY!")
+// }
 
-func (y YAZ) StepOnGas() {
-	fmt.Println("Я YAZik! 22222!")
-}
-func (y YAZ) StepOnBrake() {
-	magenta := color.New(color.FgHiMagenta).SprintFunc()
-	fmt.Println(" ")
-	fmt.Println(magenta("YAZ braking yyyyyyyyy"))
-}
-func (b YAZ) Bibi() {
-	green := color.New(color.BgHiGreen).SprintFunc()
-	fmt.Println(green(" ----  Я Буханка! bibibibibi!"))
-}
 func ride(auto Auto) {
 	red := color.New(color.FgHiRed).SprintFunc()
 
 	fmt.Println(red("Я водила с нижнего тагила"))
 	fmt.Println("Я садюсь в авто")
 	fmt.Println("Я жму на газ")
-
-	auto.Bibi()
 	auto.StepOnGas()
-	auto.StepOnBrake()
-	
-
 }
 
 func main() {
+	bmw := BMW{}
+	skoda := Skoda{}
 
-	sk:= Skoda{}
-	yaz := YAZ{}
-
-	ride(sk)
-	
-	fmt.Println("")
-	ride(yaz)
-
+	// skoda.StepOnGas()
+	ride(bmw)
+	ride(skoda)
 }
