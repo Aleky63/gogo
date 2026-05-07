1ef91b08295ad455a56c8ef1f2bcfa3533b34012
Aleksey <134486666+Aleky63@users.noreply.github.com>
2026-05-07 17:44:15 +0300

    02_07.05

--- newgo2/go8/main.go
+++ newgo2/go8/main.go
@@ -1,12 +1,58 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"time"
 
 	"github.com/fatih/color"
 )
 
+var (
+	red   = color.New(color.FgHiRed).SprintFunc()
+	green = color.New(color.FgHiGreen).SprintFunc()
+)
+
+func foo(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("endEnd_foo")
+			return
+		default:
+			fmt.Println(red("maksim"))
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func boo(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("endEnd_boo")
+			return
+		default:
+			fmt.Println("kat")
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+}
+
 func main() {
-	red := color.New(color.FgHiRed).SprintFunc()
-	fmt.Println(red("maksim"))
+	parentContext, parentCancel := context.WithCancel(context.Background())
+	childContext, childCancel := context.WithCancel(parentContext)
+
+	go foo(parentContext)
+	go boo(childContext)
+
+	time.Sleep(3 * time.Second)
+	childCancel()
+	time.Sleep(6 * time.Second)
+	parentCancel()
+
+	time.Sleep(1 * time.Second)
+	fmt.Println(green("END😍END😍END😍END😍END"))
 }
