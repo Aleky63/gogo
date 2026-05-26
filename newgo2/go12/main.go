debd79ca125364b4a82d27f3a5a48499b5d70e1a
Aleksey <134486666+Aleky63@users.noreply.github.com>
2026-05-26 17:53:01 +0300

    01_26.05

---
+++ newgo2/go12/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/fatih/color"
+)
+
+var (
+	likes int = 0
+	mtx   sync.RWMutex
+)
+
+func setLikes(wg *sync.WaitGroup) {
+	defer wg.Done()
+	mtx.Lock()
+	likes++
+	mtx.Unlock()
+}
+
+func getLikes(wg *sync.WaitGroup) {
+	defer wg.Done()
+	mtx.RLock()
+	_ = likes
+	mtx.RUnlock()
+}
+
+func main() {
+	red := color.New(color.FgHiRed).SprintFunc()
+	wg := &sync.WaitGroup{}
+
+	initNime := time.Now()
+	for i := 1; i <= 10000; i++ {
+		wg.Add(1)
+		go setLikes(wg)
+	}
+	for i := 0; i <= 10000; i++ {
+		wg.Add(1)
+		go getLikes(wg)
+	}
+	fmt.Println(red("TIME:", time.Since(initNime)))
+	wg.Wait()
+}
