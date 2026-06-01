58b4025ad5427332f736cb851f90cdcb76a9ec5a
Aleksey <134486666+Aleky63@users.noreply.github.com>
2026-06-01 17:00:29 +0300

    01_01.06

--- README.md
+++ README.md
@@ -3,4 +3,7 @@
 https://stepik.org/lesson/1500093/step/1?unit=1520202
 
 newgo
-https://www.youtube.com/watch?v=dpvRDJjUJf8 -->
+https://www.youtube.com/watch?v=dpvRDJjUJf8
+
+newgo2
+https://www.youtube.com/watch?v=4xST8IWJFZc
--- newgo2/go14/main.go
+++ newgo2/go14/main.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/fatih/color"
+)
+
+func payHandler(w http.ResponseWriter, r *http.Request) {
+	str := "Buy Kyiv for money!!!"
+	b := []byte(str)
+	_, err := w.Write(b)
+	if err != nil {
+		fmt.Println("ERROOOOOOOR:", err.Error())
+	} else {
+		fmt.Println("BUY_________BUY")
+	}
+}
+
+func cancelHandler(w http.ResponseWriter, r *http.Request) {
+	str := "Kyiv in three days, didn't work, cancel!!!"
+	b := []byte(str)
+	_, err := w.Write(b)
+	if err != nil {
+		fmt.Println("ERROOOOOOOR:", err.Error())
+	} else {
+		fmt.Println("CANCEL______________CANCEL")
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	str := "Kyiv in three days!!! "
+	b := []byte(str)
+	_, err := w.Write(b)
+	if err != nil {
+		fmt.Println("ERROOOOOOOR:", err.Error())
+	} else {
+		fmt.Println("OK_____________________OK")
+	}
+}
 
 func main() {
-	fmt.Println("hello")
+	http.HandleFunc("/default", handler)
+	http.HandleFunc("/pay", payHandler)
+	http.HandleFunc("/cancel", cancelHandler)
+
+	magenta := color.New(color.FgHiMagenta).SprintFunc()
+	fmt.Println(magenta("hello"))
+	fmt.Println("Запускаю")
+	err := http.ListenAndServe(":9091", nil)
+	if err != nil {
+		fmt.Println("!🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣:", err.Error())
+	}
+	fmt.Println("Закончили")
 }
