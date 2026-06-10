e7a77062f0763a7954e5d73ef42469ca2699e4bf
Aleksey <134486666+Aleky63@users.noreply.github.com>
2026-06-10 16:08:02 +0300

    01_10.06

--- newgo2/go16/main.go
+++ newgo2/go16/main.go
@@ -15,6 +15,11 @@ var (
 )
 
 func payHandler(w http.ResponseWriter, r *http.Request) {
+	for k, v := range r.Header {
+		fmt.Println("k:", k, "--v:", v)
+	}
+	fmt.Println("Http method:", r.Method)
+
 	httpRequestBody, err := io.ReadAll(r.Body)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
