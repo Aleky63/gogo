3e281bbf3f74a8185f1b768b4295d8bdf11fc924
Aleksey <134486666+Aleky63@users.noreply.github.com>
2026-06-13 17:58:51 +0300

    01_13.06

--- newgo2/go17/main.go
+++ newgo2/go17/main.go
@@ -1,14 +1,32 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"sync"
+
+	"github.com/fatih/color"
 )
 
 type Payment struct {
-	Description string
-	USD         int
+	Description string `json:"description"`
+	USD         int    `json:"usd"`
+	FullName    string `json:"fullName"`
+	Address     string `json:"address"`
+	Array       []int  `json:"array"`
+}
+
+func (p Payment) Println() {
+	magenta := color.New(color.FgHiMagenta).SprintFunc()
+	fmt.Println(magenta("================="))
+
+	fmt.Println("Description:", p.Description)
+	fmt.Println("USD:", p.USD)
+	fmt.Println("FullName:", p.FullName)
+	fmt.Println("Address:", p.Address)
+	fmt.Println("Array:", p.Array)
+	fmt.Println(magenta("================="))
 }
 
 var (
@@ -18,6 +36,23 @@ var (
 )
 
 func payHandler(w http.ResponseWriter, r *http.Request) {
+	var payment Payment
+	if err := json.NewDecoder(r.Body).Decode(&payment); err != nil {
+		fmt.Println("err:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+
+	}
+
+	mtx.Lock()
+	defer mtx.Unlock()
+	if money-payment.USD >= 0 {
+		money -= payment.USD
+	}
+	paymentHistory = append(paymentHistory, payment)
+	payment.Println()
+	fmt.Println("money:", money)
+	fmt.Println("payment histori:", paymentHistory)
 }
 
 func main() {
