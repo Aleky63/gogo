5e1bb74fcd80f0ea7812755a4c471d4e8addb83b
Aleksey <134486666+Aleky63@users.noreply.github.com>
2026-06-14 17:04:22 +0300

    01_14.06

--- newgo2/go17/main.go
+++ newgo2/go17/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"sync"
+	"time"
 
 	"github.com/fatih/color"
 )
@@ -15,6 +16,9 @@ type Payment struct {
 	FullName    string `json:"fullName"`
 	Address     string `json:"address"`
 	Array       []int  `json:"array"`
+
+	Bool bool `json:"bool"`
+	Time time.Time
 }
 
 func (p Payment) Println() {
@@ -26,6 +30,8 @@ func (p Payment) Println() {
 	fmt.Println("FullName:", p.FullName)
 	fmt.Println("Address:", p.Address)
 	fmt.Println("Array:", p.Array)
+	fmt.Println("Bool:", p.Bool)
+
 	fmt.Println(magenta("================="))
 }
 
@@ -34,6 +40,11 @@ var (
 	money          = 1000
 	paymentHistory = make([]Payment, 0)
 )
+
+type HttpResponse struct {
+	Money          int       `json:"mmmoney"`
+	PaymentHistory []Payment `json:"phistory"`
+}
 
 func payHandler(w http.ResponseWriter, r *http.Request) {
 	var payment Payment
@@ -44,15 +55,34 @@ func payHandler(w http.ResponseWriter, r *http.Request) {
 
 	}
 
+	payment.Time = time.Now()
+	payment.Println()
+
 	mtx.Lock()
 	defer mtx.Unlock()
 	if money-payment.USD >= 0 {
 		money -= payment.USD
 	}
+
 	paymentHistory = append(paymentHistory, payment)
-	payment.Println()
-	fmt.Println("money:", money)
-	fmt.Println("payment histori:", paymentHistory)
+
+	HttpResponse := HttpResponse{
+		Money: money,
+
+		PaymentHistory: paymentHistory,
+	}
+
+	b, err := json.MarshalIndent(HttpResponse, "", "	")
+	if err != nil {
+		fmt.Println("err:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if _, err := w.Write(b); err != nil {
+		fmt.Println("err:", err)
+		return
+	}
 }
 
 func main() {
