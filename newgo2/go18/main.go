5746abca750caa05f57b419ce1098f461c7e1544
Aleksey <134486666+Aleky63@users.noreply.github.com>
2026-06-16 17:20:02 +0300

    01_16.06

---
+++ newgo2/go18/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.URL.Path)
+}
+
+func main() {
+	http.HandleFunc("/tasks/", handler)
+
+	if err := http.ListenAndServe(":9091", nil); err != nil {
+		fmt.Println("err:", err)
+	}
+	fmt.Println("hello")
+}
