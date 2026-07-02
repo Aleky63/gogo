5aa3a993d06bd87e8a195a976e6ffbd8caed6706
Aleksey <134486666+Aleky63@users.noreply.github.com>
2026-07-02 14:34:11 +0300

    01_02.07

---
+++ newgo3/go1/feature1/fecture1.go
@@ -0,0 +1,7 @@
+package feature1
+
+import "fmt"
+
+func Feature1() {
+	fmt.Println("I'm Feature1😍😍😍")
+}
---
+++ newgo3/go1/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"study/feature1"
+)
+
+func main() {
+	fmt.Println("hello")
+	feature1.Feature1()
+}
