60176c5a4b9f92c15ab83e02e72d08ca65d0165f
Aleksey <134486666+Aleky63@users.noreply.github.com>
2026-07-07 17:52:03 +0300

    01_07.07

---
+++ newgo3/go2/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"study/feature1"
+	"study/feature2"
+	"study/feature_postgres/simple_connection"
+)
+
+func main() {
+	fmt.Println("hello")
+	feature1.Feature1()
+	feature2.Feature2()
+	simple_connection.CheckConnection()
+}
