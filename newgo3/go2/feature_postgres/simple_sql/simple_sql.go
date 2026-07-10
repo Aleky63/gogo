710d0ad3c6ef0a527f60f6e1a4877d87bb787f61
Aleksey <134486666+Aleky63@users.noreply.github.com>
2026-07-10 16:00:33 +0300

    01_10.07

---
+++ newgo3/go2/feature_postgres/simple_sql/simple_sql.go
@@ -0,0 +1,4 @@
+package simple_sql
+
+func CreateTable(conn *pgx.Conn) {
+}
--- newgo3/go2/main.go
+++ newgo3/go2/main.go
@@ -1,16 +1,10 @@
 package main
 
 import (
-	"fmt"
-
-	"study/feature1"
-	"study/feature2"
 	"study/feature_postgres/simple_connection"
 )
 
 func main() {
-	fmt.Println("hello")
-	feature1.Feature1()
-	feature2.Feature2()
-	simple_connection.CheckConnection()
+	ctx := contex.Background()
+	conn, err := simple_connection.CheckConnection()
 }
