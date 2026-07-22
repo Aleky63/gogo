a3a591047fd78052252f2efa86a233cfdcaa510b
Aleksey <134486666+Aleky63@users.noreply.github.com>
2026-07-22 17:58:59 +0300

    01_22.07

--- newgo3/go2/feature_postgres/simple_sql/simple_sql.go
+++ newgo3/go2/feature_postgres/simple_sql/simple_sql.go
@@ -1,4 +1,27 @@
 package simple_sql
 
-func CreateTable(conn *pgx.Conn) {
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5"
+)
+
+func CreateTable(ctx context.Context, conn *pgx.Conn) {
+	sqlQuery := `
+	CREATE TABLE tasks (
+	id SERIAL PRIMARY KEY,
+	title VARCHAR(200) NOT NULL,
+	description VARCHAR(1000) NOT NULL,
+	completed BOOLEAN NOT NULL,
+	created_at TIMESTAMP NOT NULL,
+	completed TIMESTAMP 
+		);
+	
+	
+	`
+	_, err := conn.Exec(ctx, sqlQuery)
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- newgo3/go2/main.go
+++ newgo3/go2/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"context"
+
 	"study/feature_postgres/simple_connection"
+	"study/feature_postgres/simple_sql"
 )
 
 func main() {
-	ctx := contex.Background()
-	conn, err := simple_connection.CheckConnection()
+	ctx := context.Background()
+	conn, err := simple_connection.CreateConnection(ctx)
+	if err != nil {
+		panic(err)
+	}
+	simple_sql.CreateTable(ctx, conn)
 }
