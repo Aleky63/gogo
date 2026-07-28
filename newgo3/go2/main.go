4b3a33d826d6ab45e1ce0842ced9a75bccedc40f
Aleksey <134486666+Aleky63@users.noreply.github.com>
2026-07-28 17:30:52 +0300

    01_28.07

--- newgo3/go2/main.go
+++ newgo3/go2/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"study/feature_postgres/simple_connection"
 	"study/feature_postgres/simple_sql"
@@ -44,7 +45,24 @@ func main() {
 		panic(err)
 	}
 
+	for _, task := range tasks {
+		if task.ID == 7 {
+
+			task.Title = " TRAMP😊😊😊😊😊"
+			task.Description = "The red-haired horseradish is weird"
+			task.Completed = true
+			now := time.Now()
+			task.CompletedAt = &now
+
+			if err := simple_sql.UpdateTask(ctx, conn, task); err != nil {
+				panic(err)
+			}
+			break
+		}
+	}
+
 	red := color.New(color.FgRed).SprintFunc()
+	fmt.Println(red("-----------------------"))
 	fmt.Println(red("🛠️🛠️- Succeed!-🛠️🛠️"))
 	pp.Println(tasks)
 }
